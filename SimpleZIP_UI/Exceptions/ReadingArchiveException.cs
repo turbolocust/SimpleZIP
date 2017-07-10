3b62e565b1ee34242fefc3dbe9e342bad2d7d786
Matthias Fussenegger <matfu2@gmail.com>
2017-07-10 18:56:22 +0200

    Introduced ReadingArchiveException

---
+++ SimpleZIP_UI/Exceptions/ReadingArchiveException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SimpleZIP_UI.Exceptions
+{
+    internal class ReadingArchiveException : Exception
+    {
+        public ReadingArchiveException()
+        {
+        }
+
+        public ReadingArchiveException(string message) : base(message)
+        {
+        }
+
+        public ReadingArchiveException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
