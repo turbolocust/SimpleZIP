234e59f77c8a5eacfdd49d14246f6f6bbfc08893
Matthias Fussenegger <matfu2@gmail.com>
2017-07-10 18:52:21 +0200

    Separate method for GZip compression

--- SimpleZIP_UI/Presentation/Control/CompressionSummaryPageControl.cs
+++ SimpleZIP_UI/Presentation/Control/CompressionSummaryPageControl.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Storage;
 using Windows.UI.Xaml.Controls;
 using SimpleZIP_UI.Application.Compression;
 using SimpleZIP_UI.Application.Compression.Model;
@@ -27,47 +29,17 @@ namespace SimpleZIP_UI.Presentation.Control
             var selectedFiles = operationInfo.SelectedFiles;
             var key = operationInfo.ArchiveType;
             var resultMessage = new StringBuilder();
-            var statusCode = Result.Status.Fail;
+            Result.Status statusCode;
 
             CheckFileSizes(selectedFiles);
 
             try
             {
-                if (key.Equals(Archives.ArchiveType.GZip)) // requires special treatment
-                {
-                    var subMessage = new StringBuilder();
-                    var successCount = 0;
-                    // compress each file separately
-                    foreach (var file in selectedFiles)
-                    {
-                        if (IsCancelRequest) break;
-
-                        operationInfo.SelectedFiles = new[] { file };
-                        var subResult = await Operation.Perform(operationInfo);
-
-                        if (subResult.StatusCode != Result.Status.Success)
-                        {
-                            statusCode = Result.Status.PartialFail;
-                            subMessage.AppendLine("File ").Append(file.DisplayName).Append(" was not compressed.");
-                        }
-                        else
-                        {
-                            ++successCount;
-                        }
-                        subMessage.AppendLine(subResult.Message);
-                    }
-                    if (successCount == selectedFiles.Count)
-                    {
-                        statusCode = Result.Status.Success;
-                    }
-                    resultMessage.Append(subMessage);
-                }
-                else
-                {
-                    var subResult = await Operation.Perform(operationInfo);
-                    resultMessage.Append(subResult.Message);
-                    statusCode = subResult.StatusCode;
-                }
+                var result = key.Equals(Archives.ArchiveType.GZip) // requires special treatment
+                    ? await CompressGZip(selectedFiles, operationInfo)
+                    : await Operation.Perform(operationInfo);
+                resultMessage.Append(result.Message);
+                statusCode = result.StatusCode;
             }
             catch (Exception ex)
             {
@@ -87,6 +59,49 @@ namespace SimpleZIP_UI.Presentation.Control
             {
                 StatusCode = statusCode,
                 Message = resultMessage.ToString()
+            };
+        }
+
+        /// <summary>
+        /// Compresses each of the specified files as GZip since GZip 
+        /// only allows the compression of a single file.
+        /// </summary>
+        /// <param name="files">The files to be compressed.</param>
+        /// <param name="operationInfo">The amount of operations to be performed.</param>
+        /// <returns>A result object consisting of further details.</returns>
+        private async Task<Result> CompressGZip(IReadOnlyCollection<StorageFile> files, CompressionInfo operationInfo)
+        {
+            var subMessage = new StringBuilder();
+            var successCount = 0;
+            var statusCode = Result.Status.Fail;
+
+            foreach (var file in files) // compress each file separately
+            {
+                if (IsCancelRequest) break;
+
+                operationInfo.SelectedFiles = new[] { file };
+                var subResult = await Operation.Perform(operationInfo);
+
+                if (subResult.StatusCode != Result.Status.Success)
+                {
+                    statusCode = Result.Status.PartialFail;
+                    subMessage.AppendLine("File ").Append(file.DisplayName).Append(" was not compressed.");
+                }
+                else
+                {
+                    ++successCount;
+                }
+                subMessage.AppendLine(subResult.Message);
+            }
+            if (successCount == files.Count)
+            {
+                statusCode = Result.Status.Success;
+            }
+
+            return new Result
+            {
+                StatusCode = statusCode,
+                Message = subMessage.ToString()
             };
         }
     }
