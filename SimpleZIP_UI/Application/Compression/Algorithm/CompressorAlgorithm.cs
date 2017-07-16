91caed0dc15eeaf3c974741d6f4936e961c81292
Matthias Fussenegger <matfu2@gmail.com>
2017-07-16 11:57:00 +0200

    Added workaround for GZip file renaming

--- SimpleZIP_UI/Application/Compression/Algorithm/CompressorAlgorithm.cs
+++ SimpleZIP_UI/Application/Compression/Algorithm/CompressorAlgorithm.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
 using Windows.Storage;
+using SharpCompress.Compressors.Deflate;
 using SharpCompress.Readers;
 using SharpCompress.Writers;
 using SimpleZIP_UI.Application.Compression.Model;
@@ -47,6 +48,7 @@ namespace SimpleZIP_UI.Application.Compression.Algorithm
                         await outputStream.WriteAsync(bytes, 0, readBytes, Token);
                     }
                 }
+                GZipOutputFileNameWorkaround(file, stream);
             }
             return true;
         }
@@ -80,6 +82,27 @@ namespace SimpleZIP_UI.Application.Compression.Algorithm
                 }
             }
             return true;
+        }
+
+        /// <summary>
+        /// This method only exists to provide a workaround for renaming a file to its
+        /// filename as it should be defined in the GZip header. The SharpCompress library 
+        /// only sets the filename after the first call of <see cref="Stream.Read"/> and 
+        /// not when the stream instance is actually constructed like most other libraries do.
+        /// 
+        /// In a perfect world, the filename would be considered when calling the abstract
+        /// method <see cref="GetCompressorStream"/> as this method opens the stream of
+        /// the archive file. The name could then be set to its corresponding property in
+        /// <see cref="CompressorOptions"/> for data exchange.
+        /// </summary>
+        /// <param name="file">The file to be renamed.</param>
+        /// <param name="stream">The possible <see cref="GZipStream"/> which holds the filename.</param>
+        private static async void GZipOutputFileNameWorkaround(IStorageItem file, Stream stream)
+        {
+            if (stream is GZipStream gzipStream && !string.IsNullOrEmpty(gzipStream.FileName))
+            {
+                await file.RenameAsync(gzipStream.FileName);
+            }
         }
 
         /// <summary>
