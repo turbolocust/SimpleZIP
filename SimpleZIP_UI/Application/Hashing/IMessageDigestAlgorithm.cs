eaa023186c7b5ec1d35c50879ccb2bbb390d8c69
Matthias Fussenegger <matfu2@gmail.com>
2017-09-11 18:55:55 +0200

    Added message digest algorithm

---
+++ SimpleZIP_UI/Application/Hashing/IMessageDigestAlgorithm.cs
@@ -0,0 +1,17 @@
+﻿using Windows.Security.Cryptography.Core;
+
+namespace SimpleZIP_UI.Application.Hashing
+{
+    public interface IMessageDigestAlgorithm
+    {
+        /// <summary>
+        /// Calculates a hash value using the specified message digest algorithm.
+        /// See <see cref="HashAlgorithmNames"/> for the correct string representations.
+        /// </summary>
+        /// <param name="data">The data to be hashed.</param>
+        /// <param name="algorithmName">The name of the message digest algorithm.</param>
+        /// <param name="hashString">Hexadecimal string representation (in upper case) of the hash value.</param>
+        /// <returns>The hashed data.</returns>
+        byte[] CalculateHashValue(byte[] data, string algorithmName, out string hashString);
+    }
+}
---
+++ SimpleZIP_UI/Application/Hashing/MessageDigestAlgorithm.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SimpleZIP_UI.Application.Hashing
+{
+    internal class MessageDigestAlgorithm : IMessageDigestAlgorithm
+    {
+        /// <inheritdoc />
+        public byte[] CalculateHashValue(byte[] data, string algorithmName, out string hashString)
+        {
+            var algorithm = GetHashAlgorithm(algorithmName);
+            var hashedData = algorithm.ComputeHash(data);
+            hashString = ConvertHashValueToString(hashedData);
+            return hashedData;
+        }
+
+        private HashAlgorithm GetHashAlgorithm(string name)
+        {
+            HashAlgorithm algorithm;
+            switch (name)
+            {
+                case "MD5":
+                    algorithm = MD5.Create();
+                    break;
+                case "SHA1":
+                    algorithm = SHA1.Create();
+                    break;
+                case "SHA256":
+                    algorithm = SHA256.Create();
+                    break;
+                case "SHA384":
+                    algorithm = SHA384.Create();
+                    break;
+                case "SHA512":
+                    algorithm = SHA512.Create();
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(name), name, null);
+            }
+            return algorithm;
+        }
+
+        private static string ConvertHashValueToString(IEnumerable<byte> hashedData)
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (var byteVal in hashedData)
+            {
+                // convert to upper case hex format
+                stringBuilder.AppendFormat("{0:X2}", byteVal);
+            }
+            return stringBuilder.ToString();
+        }
+    }
+}
