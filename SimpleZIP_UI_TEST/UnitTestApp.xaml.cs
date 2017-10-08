c53bb7802ec149f8e041aa0d70f96735b22edd44
Matthias Fussenegger <matfu2@gmail.com>
2017-10-08 15:25:11 +0200

    Added license header to unit test files

--- SimpleZIP_UI_TEST/UnitTestApp.xaml.cs
+++ SimpleZIP_UI_TEST/UnitTestApp.xaml.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿// ==++==
+// 
+// Copyright (C) 2017 Matthias Fussenegger
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// ==--==
+using System;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.UI.Xaml;
@@ -7,11 +25,13 @@ using Windows.UI.Xaml.Navigation;
 
 namespace SimpleZIP_UI_TEST
 {
+    /// <inheritdoc />
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
     internal sealed partial class App
     {
+        /// <inheritdoc />
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
@@ -70,7 +90,7 @@ namespace SimpleZIP_UI_TEST
         /// </summary>
         /// <param name="sender">The Frame which failed navigation</param>
         /// <param name="e">Details about the navigation failure</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        private static void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
         {
             throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
         }
@@ -82,7 +102,7 @@ namespace SimpleZIP_UI_TEST
         /// </summary>
         /// <param name="sender">The source of the suspend request.</param>
         /// <param name="e">Details about the suspend request.</param>
-        private void OnSuspending(object sender, SuspendingEventArgs e)
+        private static void OnSuspending(object sender, SuspendingEventArgs e)
         {
             var deferral = e.SuspendingOperation.GetDeferral();
             //TODO: Save application state and stop any background activity
