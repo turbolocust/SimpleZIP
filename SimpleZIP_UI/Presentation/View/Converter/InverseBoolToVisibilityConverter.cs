f6bf88163e9f68add78bf39398b08f807c5d9cba
Matthias Fussenegger <matfu2@gmail.com>
2018-07-11 16:18:15 +0200

    Introduce InverseBoolToVisibilityConverter.cs

---
+++ SimpleZIP_UI/Presentation/View/Converter/InverseBoolToVisibilityConverter.cs
@@ -0,0 +1,48 @@
+﻿// ==++==
+// 
+// Copyright (C) 2018 Matthias Fussenegger
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// ==--==
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace SimpleZIP_UI.Presentation.View.Converter
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Converter which converts a negated boolean to <see cref="Visibility"/>.
+    /// </summary>
+    internal class InverseBoolToVisibilityConverter : IValueConverter
+    {
+        /// <inheritdoc />
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (!(bool)value)
+            {
+                return Visibility.Visible;
+            }
+            return Visibility.Collapsed;
+        }
+
+        /// <inheritdoc />
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            var visibility = (Visibility)value;
+            return visibility != Visibility.Visible;
+        }
+    }
+}
