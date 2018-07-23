a0a082290a0d14d30e4c367bc4e0b85997f74bf6
Matthias Fussenegger <matfu2@gmail.com>
2018-07-23 16:07:14 +0200

    Advanced check for archive format

--- SimpleZIP_UI/Application/Compression/Archives.cs
+++ SimpleZIP_UI/Application/Compression/Archives.cs
@@ -1,6 +1,6 @@
 ﻿// ==++==
 // 
-// Copyright (C) 2017 Matthias Fussenegger
+// Copyright (C) 2018 Matthias Fussenegger
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -24,21 +24,20 @@ using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.Storage;
+using SharpCompress.Archives.Rar;
+using SharpCompress.Common;
+using SharpCompress.Readers;
 
 namespace SimpleZIP_UI.Application.Compression
 {
     internal static class Archives
     {
-        private const int Rar5HeaderSize = 8;
-
-        private const string Rar5HeaderSignature = "526172211A070100";
-
         /// <summary>
         /// Enumeration to identify archive types.
         /// </summary>
         public enum ArchiveType
         {
-            Zip, GZip, BZip2, LZip, Tar, TarGz, TarBz2, TarLz, Rar
+            Unknown, Zip, GZip, BZip2, LZip, Tar, TarGz, TarBz2, TarLz, Rar
         }
 
         /// <summary>
@@ -52,6 +51,11 @@ namespace SimpleZIP_UI.Application.Compression
         /// file types with multiple file name extensions.
         /// </summary>
         internal static readonly IDictionary<string, ArchiveType> ArchiveExtendedFileTypes;
+
+        /// <summary>
+        /// Maps <see cref="SharpCompress.Common.ArchiveType"/> to <see cref="ArchiveType"/>.
+        /// </summary>
+        internal static readonly IDictionary<SharpCompress.Common.ArchiveType, ArchiveType> ArchiveTypes;
 
         static Archives()
         {
@@ -84,6 +88,14 @@ namespace SimpleZIP_UI.Application.Compression
                 { ".tar.lzip", ArchiveType.TarLz },
                 { ".tar.lzma", ArchiveType.TarLz }
             };
+            // populate dictionary which maps third-party archive types to local ones
+            ArchiveTypes = new Dictionary<SharpCompress.Common.ArchiveType, ArchiveType>
+            {
+                { SharpCompress.Common.ArchiveType.Zip, ArchiveType.Zip },
+                { SharpCompress.Common.ArchiveType.Tar, ArchiveType.Tar },
+                { SharpCompress.Common.ArchiveType.Rar, ArchiveType.GZip },
+                { SharpCompress.Common.ArchiveType.GZip, ArchiveType.GZip }
+            };
         }
 
         /// <summary>
@@ -95,6 +107,7 @@ namespace SimpleZIP_UI.Application.Compression
         internal static ICompressionAlgorithm DetermineAlgorithm(ArchiveType value)
         {
             ICompressionAlgorithm algorithm;
+            // ReSharper disable once SwitchStatementMissingSomeCases
             switch (value)
             {
                 case ArchiveType.Zip:
@@ -131,34 +144,92 @@ namespace SimpleZIP_UI.Application.Compression
         }
 
         /// <summary>
-        /// Checks whether the file is a RAR4 or RAR5 archive.
+        /// 
         /// </summary>
-        /// <param name="file">The file to be checked.</param>
-        /// <returns>True if file is RAR4 or RAR5 archive, false otherwise.</returns>
-        internal static async Task<bool> IsRarArchive(StorageFile file)
+        /// <param name="file">The archive to be checked.</param>
+        /// <param name="password">The password of the file if encrypted.</param>
+        /// <returns></returns>
+        /// <exception cref="CryptographicException">
+        /// Thrown if no or wrong password has been provided.</exception>
+        /// <exception cref="InvalidArchiveTypeException">
+        /// Thrown if archive type is not supported.</exception>
+        internal static async Task<ArchiveType> DetermineArchiveType(StorageFile file, string password = null)
         {
-            bool isRarArchive;
+            var archiveType = ArchiveType.Unknown;
+
             try
             {
+                // let SharpCompress' reader do the archive detection
+                var options = new ReaderOptions { Password = password };
                 using (var stream = await file.OpenStreamForReadAsync())
+                using (var reader = ReaderFactory.Open(stream, options))
                 {
-                    isRarArchive = SharpCompress.Archives.Rar.RarArchive.IsRarFile(stream);
-                }
-                if (!isRarArchive) // check if RAR5 format
-                {
-                    using (var stream = await file.OpenStreamForReadAsync())
+                    var type = reader.ArchiveType;
+                    if (ArchiveTypes.TryGetValue(type, out var localType))
                     {
-                        var buffer = new byte[Rar5HeaderSize];
-                        var readBytes = stream.Read(buffer, 0, Rar5HeaderSize);
-                        if (readBytes > 0)
+                        archiveType = localType;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex.GetType() == typeof(CryptographicException))
+                {
+                    throw; // just re-throw
+                }
+
+                throw new InvalidArchiveTypeException("Archive type is unknown or not supported.", ex);
+            }
+
+            return archiveType;
+        }
+
+        /// <summary>
+        /// Checks whether the file is a RAR4 or RAR5 archive. The specified
+        /// stream is expected to be seekable. Only then the check for
+        /// RAR5 will be performed in addition to the RAR4 check (if not RAR4).
+        /// Otherwise the check for the RAR5 format will only be performed
+        /// if explicitly set via the optional parameter.
+        /// </summary>
+        /// <param name="stream">Archive stream to be checked.</param>
+        /// <param name="password">The password of the file if encrypted.</param>
+        /// <param name="rar5Only">True to only check for RAR5 format, false otherwise.</param>
+        /// <returns></returns>
+        internal static bool IsRarArchive(Stream stream,
+            string password = null, bool rar5Only = false)
+        {
+            const int rar5HeaderSize = 8; // bytes
+            const string rar5HeaderSignature = "526172211A070100";
+
+            bool isRarArchive = false;
+            try
+            {
+                if (!rar5Only)
+                {
+                    isRarArchive = RarArchive.IsRarFile(stream,
+                        new ReaderOptions
                         {
-                            var header = new StringBuilder(Rar5HeaderSize * 2);
-                            foreach (var value in buffer)
-                            {
-                                header.AppendFormat("{0:X2}", value); // convert to HEX
-                            }
-                            isRarArchive = header.ToString().Equals(Rar5HeaderSignature);
+                            Password = password,
+                            LeaveStreamOpen = true
+                        });
+                }
+                // check if RAR5 format if not RAR4
+                if (rar5Only || !isRarArchive && stream.CanSeek)
+                {
+                    if (!rar5Only)
+                    {
+                        stream.Seek(0, SeekOrigin.Begin); // reset stream
+                    }
+                    var buffer = new byte[rar5HeaderSize];
+                    var readBytes = stream.Read(buffer, 0, rar5HeaderSize);
+                    if (readBytes > 0)
+                    {
+                        var header = new StringBuilder(rar5HeaderSize * 2);
+                        foreach (var value in buffer)
+                        {
+                            header.AppendFormat("{0:X2}", value); // convert to HEX
                         }
+                        isRarArchive = header.ToString().Equals(rar5HeaderSignature);
                     }
                 }
             }
@@ -167,6 +238,35 @@ namespace SimpleZIP_UI.Application.Compression
                 isRarArchive = false;
             }
             return isRarArchive;
+        }
+
+        /// <summary>
+        /// Checks whether the specified file is a RAR4 or RAR5 archive.
+        /// </summary>
+        /// <param name="file">The archive to be checked.</param>
+        /// <param name="password">The password of the file if encrypted.</param>
+        /// <returns>True if file is RAR4 or RAR5 archive, false otherwise.</returns>
+        internal static async Task<bool> IsRarArchive(StorageFile file, string password = null)
+        {
+            using (var stream = await file.OpenStreamForReadAsync())
+            {
+                if (!stream.CanSeek)
+                {
+                    bool isRarArchive;
+                    if (!(isRarArchive = IsRarArchive(stream, password)))
+                    {
+                        // not RAR4, hence check for RAR5 only
+                        using (var stream2 = await file.OpenStreamForReadAsync())
+                        {
+                            isRarArchive = IsRarArchive(stream2, password, true);
+                        }
+                    }
+
+                    return isRarArchive;
+                }
+
+                return IsRarArchive(stream, password);
+            }
         }
     }
 }
