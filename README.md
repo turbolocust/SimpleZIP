d612c19992bd87b2350ef6281cadd96dadd2d86b
Matthias Fussenegger <matfu2@gmail.com>
2018-07-26 15:20:12 +0200

    Introduce INavigation.cs, IGUIController.cs

--- README.md
+++ README.md
@@ -35,7 +35,7 @@ Supported message digest algorithms:
 <br />
 At the current state, this application supports encrypted archives of type ZIP (legacy encryption) and RAR4 (fully encrypted).
 <br /><br />
-This application uses the <a href="https://github.com/adamhathcock/sharpcompress">SharpCompress</a> library and depends on its quality when it comes to compression and decompression. Unit tests exist to test updates of this library (see project _SimpleZIP_UI_TEST_). 
+This application uses the <a href="https://github.com/adamhathcock/sharpcompress">SharpCompress</a> library and depends on its quality when it comes to compression and decompression. Unit tests exist to test updates of this library (see project SimpleZIP_UI_TEST). 
 <br />
 
 # Development requirements
---
+++ SimpleZIP_UI/Presentation/Controller/IGUIController.cs
@@ -0,0 +1,32 @@
+﻿// ==++==
+// 
+// Copyright (C) 2018 Matthias Fussenegger
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// ==--==
+
+namespace SimpleZIP_UI.Presentation.Controller
+{
+    /// <summary>
+    /// Defines the minimum contract for a GUI controller.
+    /// </summary>
+    internal interface IGuiController
+    {
+        /// <summary>
+        /// Aggregated navigation handler for navigation between pages.
+        /// </summary>
+        INavigation Navigation { get; }
+    }
+}
