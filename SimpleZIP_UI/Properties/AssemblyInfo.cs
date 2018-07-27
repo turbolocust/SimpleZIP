cb986ea38f0f0479c03cb7baa2c83e71ee4b730e
Matthias Fussenegger <matfu2@gmail.com>
2018-07-27 09:35:22 +0200

    Update project setup

--- SimpleZIP_UI/Properties/AssemblyInfo.cs
+++ SimpleZIP_UI/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Matthias Fussenegger")]
 [assembly: AssemblyProduct("SimpleZIP_UI")]
-[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
