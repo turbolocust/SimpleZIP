49b1170825286544b782e85c1218d1a86e019493
Matthias Fussenegger <matfu2@gmail.com>
2018-08-04 09:21:37 +0200

    New compressor classes for string compression

---
+++ SimpleZIP_UI/Application/Compression/IStringCompressor.cs
@@ -0,0 +1,96 @@
+﻿// ==++==
+// 
+// Copyright (C) 2018 Matthias Fussenegger
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// ==--==
+
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+using SimpleZIP_UI.Application.Util;
+
+namespace SimpleZIP_UI.Application.Compression
+{
+    internal interface IStringCompressor : ICompressor<string, string>
+    {
+    }
+
+    /// <summary>
+    /// Compresses and decompresses strings using a GZIP compressor.
+    /// </summary>
+    internal class StringGzipCompressor : IStringCompressor
+    {
+        private readonly Encoding _encoding;
+
+        /// <summary>
+        /// Constructes a new instance of this class.
+        /// </summary>
+        /// <param name="charEncoding">Character encoding of strings
+        /// to be processed by this instance. Defaults to
+        /// <see cref="Encoding.UTF8"/>.</param>
+        public StringGzipCompressor(Encoding charEncoding = null)
+        {
+            _encoding = charEncoding ?? Encoding.UTF8;
+        }
+
+        /// <summary>
+        /// Compresses the specified string.
+        /// </summary>
+        /// <param name="value">The string to be compressed.</param>
+        /// <returns>BASE64 encoded string representation of compressed data.</returns>
+        public string Compress(string value)
+        {
+            string base64;
+
+            using (var inputStream = value.ToStream(_encoding))
+            using (var outputStream = new MemoryStream())
+            {
+                using (var gzipStream = new GZipStream(outputStream, CompressionMode.Compress, true))
+                {
+                    inputStream.CopyTo(gzipStream);
+                }
+                var compressed = outputStream.ToArray();
+                base64 = Convert.ToBase64String(compressed);
+            }
+
+            return base64;
+        }
+
+        /// <summary>
+        /// Decompresses the specified string.
+        /// </summary>
+        /// <param name="input">BASE64 encoded string representation of compressed data.</param>
+        /// <returns>The decompressed string.</returns>
+        public string Decompress(string input)
+        {
+            string output;
+
+            var compressed = Convert.FromBase64String(input);
+
+            using (var inputStream = new MemoryStream(compressed))
+            using (var gzipStream = new GZipStream(inputStream, CompressionMode.Decompress))
+            using (var outputStream = new MemoryStream())
+            {
+                gzipStream.CopyTo(outputStream);
+                var decompressed = outputStream.ToArray();
+                output = _encoding.GetString(decompressed);
+            }
+
+            return output;
+        }
+    }
+}
