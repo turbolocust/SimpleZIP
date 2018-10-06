056048a26b9b2dee91276904f349c59b682c5a26
Matthias Fussenegger <matfu2@gmail.com>
2018-10-06 12:32:44 +0200

    Add RootNodeCacheHandler.cs and ICacheHandler.cs

---
+++ SimpleZIP_UI/Presentation/Handler/ICacheHandler.cs
@@ -0,0 +1,51 @@
+﻿// ==++==
+// 
+// Copyright (C) 2018 Matthias Fussenegger
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// ==--==
+using System.Collections.Generic;
+
+namespace SimpleZIP_UI.Presentation.Handler
+{
+    internal interface ICacheHandler<T>
+    {
+        /// <summary>
+        /// The actual cache.
+        /// </summary>
+        IReadOnlyDictionary<string, T> Cache { get; }
+
+        /// <summary>
+        /// Writes a new value to the cache using the specified key.
+        /// </summary>
+        /// <param name="key">The key to be associated with the value.</param>
+        /// <param name="node">The value to be cached.</param>
+        void WriteToCache(string key, T node);
+
+
+        /// <summary>
+        /// Reads a value from the cache. If the value with the specified
+        /// key is not found, then <code>null</code> is returned.
+        /// </summary>
+        /// <param name="key">The key of the cached value.</param>
+        /// <returns>The cached value or <code>null</code>.</returns>
+        T ReadFromCache(string key);
+
+        /// <summary>
+        /// Clears the entire cache.
+        /// </summary>
+        void ClearCache();
+    }
+}
---
+++ SimpleZIP_UI/Presentation/Handler/RootNodeCacheHandler.cs
@@ -0,0 +1,77 @@
+﻿// ==++==
+// 
+// Copyright (C) 2018 Matthias Fussenegger
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// ==--==
+
+using SimpleZIP_UI.Application.Compression.Reader;
+using System.Collections.Generic;
+
+namespace SimpleZIP_UI.Presentation.Handler
+{
+    internal sealed class RootNodeCacheHandler : ICacheHandler<RootNode>
+    {
+        private readonly Dictionary<string, RootNode> _nodesCache;
+
+        /// <inheritdoc />
+        public IReadOnlyDictionary<string, RootNode> Cache => _nodesCache;
+
+        /// <inheritdoc />
+        public void WriteToCache(string key, RootNode node)
+        {
+            _nodesCache.Add(key, node);
+        }
+
+        /// <inheritdoc />
+        public RootNode ReadFromCache(string key)
+        {
+            _nodesCache.TryGetValue(key, out var rootNode);
+            return rootNode; // can be null
+        }
+
+        /// <inheritdoc />
+        public void ClearCache()
+        {
+            _nodesCache.Clear();
+        }
+
+        #region Singleton members
+        private static readonly object LockObject = new object();
+
+        private static RootNodeCacheHandler _instance;
+        public static RootNodeCacheHandler Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (LockObject)
+                    {
+                        _instance = new RootNodeCacheHandler();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+
+        private RootNodeCacheHandler()
+        {
+            _nodesCache = new Dictionary<string, RootNode>();
+        }
+        #endregion
+    }
+}
